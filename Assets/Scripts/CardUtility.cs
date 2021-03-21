81eb472df895b1d18bd27b62e00e0532a10d245f
CodySelman <codylselman@gmail.com>
2021-03-20 18:04:47 -0700

    init static CardUtility class

---
+++ Assets/Scripts/CardUtility.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CardUtility
+{
+    public static void Test() {
+        Debug.Log("Test");
+    }
+}
